af3ed92a82c89784102c4e5e4cc218f2ae9777a8
Gustavo <alexandresg333@gmail.com>
2025-08-22 11:48:01 -0300

    Inicio da tentativa do 1353

---
+++ 1353/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+const int limit = 20;
+
+// valor maximo pra o primeiror elemento = n - ( k + 1 )
+
+int main(int argc, char *argv[]) {
+  int n, k;
+  for (int i = 0; i < limit; i++) {
+    cin >> n >> k;
+  }
+  return 0;
+}
